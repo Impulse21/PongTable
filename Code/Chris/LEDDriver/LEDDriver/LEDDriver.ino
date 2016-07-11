f49a2b0636e44629f18c49444cd27253f393b9a9
Chris <dipaolo21@gmail.com>
2016-07-11 18:23:42 -0400

    Added Project

---
+++ Code/Chris/LEDDriver/LEDDriver/LEDDriver.ino
@@ -0,0 +1,18 @@
+/*
+ Name:		LEDDriver.ino
+ Created:	7/11/2016 6:18:13 PM
+ Author:	dipao
+*/
+
+const int clockPin = 13;
+const int resetPin = 12;
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
