255ad6da3d3dc5613c49a9207568d1afc46512ac
Chris <dipaolo21@gmail.com>
2016-07-11 20:13:56 -0400

    Updated Code

--- Code/Chris/LEDDriver/LEDDriver/LEDDriver.ino
+++ Code/Chris/LEDDriver/LEDDriver/LEDDriver.ino
@@ -4,8 +4,22 @@
  Author:	dipao
 */
 
-const int clockPin = 13;
+/* Total Pins: 13		*/
+
+/* Shift Registers		*/
+/* Shift Register 1: holds row 1-8 using Port B*/
+/* Shift Register 2: Holders row 9-12 */
+
+const int dataPin = 9;
+const int latchPin = 10;
+const int clockPin = 11;
 const int resetPin = 12;
+
+/** Counter pins		*/
+const int clockPin = 9;
+const int resetPin = 10;
+
+
 
 // the setup function runs once when you press reset or power the board
 void setup() {
