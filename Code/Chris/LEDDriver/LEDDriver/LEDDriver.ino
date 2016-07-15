864c5494556e991bc219cc812cfc85be978a9f17
cdipaolo <dipaolo21@gmail.com>
2016-07-15 12:55:47 -0400

    Added Pins info

--- Code/Chris/LEDDriver/LEDDriver/LEDDriver.ino
+++ Code/Chris/LEDDriver/LEDDriver/LEDDriver.ino
@@ -4,29 +4,42 @@
  Author:	dipao
 */
 
-/* Total Pins: 13		*/
+/* Total Pins: 13								*/
 
-/* Shift Registers		*/
-/* Shift Register 1: holds row 1-8 using Port B*/
-/* Shift Register 2: Holders row 9-12 */
+/* Columns (1-32)								*/
+/* Shift Register 1: holds row 1-8 using Port B	*/
+/* Shift Register 2: Holders row 9-12			*/
+const int cDataPin	= 8;
+const int cLatchPin = 9;
+const int cClockPin = 10;
 
-const int dataPin = 9;
-const int latchPin = 10;
-const int clockPin = 11;
-const int resetPin = 12;
+/* Rows (1-12)									*/
+/*   - Rows 1-8: Shift Registers				*/
+/*	 - Rows 9-12: Shift Register Q0, Q1, Q2		*/
 
-/** Counter pins		*/
-const int clockPin = 9;
-const int resetPin = 10;
-
+const int rDataPin	= 13;
+const int rLatchPin = 12;
+const int rClockPin	= 11;
 
 
 // the setup function runs once when you press reset or power the board
 void setup() {
+	// Set output pins
+	pinMode(cDataPin, OUTPUT);
+	pinMode(cLatchpin, OUTPUT);
+	pinMode(cClockPin, OUTPUT);
 
+	pinMode(rDataPin, OUTPUT);
+	pinMode(rLatchPin, OUTPUT);
+	pinMode(rClockPin, OUTPUT);
 }
 
 // the loop function runs over and over again until power down or reset
 void loop() {
   
+}
+
+void displayPattern(int loops)
+{
+
 }
